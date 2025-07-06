6d852371af55a1e63dd73774610c814b08f553a9
Jonah Aney <jonahaney@gmail.com>
2025-07-06 18:30:24 -0500

    Updated module-9

--- module-9/Aneycsd402Mod9/src/Program1.java
+++ module-9/Aneycsd402Mod9/src/Program1.java
@@ -6,20 +6,7 @@ For this assignment, you will create two different programs. Follow the instruct
 Then ask a user which element they would like to see again. Then, attempt printing the element in a try/catch format which will result in the element being displayed. 
 If the element value received is invalid, display a message that an Exception has been thrown displaying “Out of Bounds”. 
 In this program, include the use of Autoboxing/Auto-Unboxing, working with a user String input.
-    Program 2: Write a program to create a new file titled data.file, if the file does not exist. 
-Then write to the new file, adding 10 randomly generated numbers, or append 10 randomly generated numbers to a previous file. 
-Each integer is to be separated by a space. Close the file, then reopen the file and read the data from the file and display it.
 */
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Jonah
- */
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -28,41 +15,41 @@ public class Program1 {
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<>();
         
-// Add 10 strings
-        words.add("Apple");
-        words.add("Banana");
-        words.add("Cherry");
-        words.add("Date");
-        words.add("Elderberry");
-        words.add("Fig");
-        words.add("Grape");
-        words.add("Honeydew");
-        words.add("Kiwi");
-        words.add("Lemon");
+// Add 10 strings - Batman villains
+        words.add("Joker");
+        words.add("Penguin");
+        words.add("Riddler");
+        words.add("Two-Face");
+        words.add("Scarecrow");
+        words.add("Bane");
+        words.add("Poison Ivy");
+        words.add("Mr. Freeze");
+        words.add("Mad Hatter");
+        words.add("Killer Croc");
         
 // Print all elements using for-each loop
-        System.out.println("Here are the elements in the list:");
+        System.out.println("Here are the top 10 Batman villians:");
         for (String word : words) {
             System.out.println(word);
         }
         
         Scanner scanner = new Scanner(System.in);
         
-// Ask the user which index they want to see again
-        System.out.print("Enter the index (0-9) of the element you want to see again: ");
+// Ask the user which index (villian) they want to see again
+        System.out.print("Enter the index (0-9) of the villian you want to see again: ");
         String input = scanner.nextLine();
         
         try {
 // Auto-unboxing: converting String input to int
-            int index = Integer.parseInt(input); // auto-unboxing from String to int
+            int index = Integer.parseInt(input); 
             
             String selectedWord = words.get(index);
             System.out.println("Element at index " + index + ": " + selectedWord);
             
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("Exception thrown: Out of Bounds");
+            System.out.println("Out of Bounds");
         } catch (NumberFormatException e) {
-            System.out.println("Invalid input. Please enter a valid integer index.");
+            System.out.println("Invalid input. Please enter a valid number index.");
         } finally {
             scanner.close();
         }
--- module-9/Aneycsd402Mod9/src/Program2.java
+++ module-9/Aneycsd402Mod9/src/Program2.java
@@ -2,24 +2,11 @@
 Jonah Aney 07/06/25 CSD-402 Module 9
 For this assignment, you will create two different programs. Follow the instructions for each below.
 
-    Program 1: Write a program that uses an ArrayList filled with a minimum of 10 Strings. Use a ‘for-each’ loop to print the ArrayList collection. 
-Then ask a user which element they would like to see again. Then, attempt printing the element in a try/catch format which will result in the element being displayed. 
-If the element value received is invalid, display a message that an Exception has been thrown displaying “Out of Bounds”. 
-In this program, include the use of Autoboxing/Auto-Unboxing, working with a user String input.
     Program 2: Write a program to create a new file titled data.file, if the file does not exist. 
 Then write to the new file, adding 10 randomly generated numbers, or append 10 randomly generated numbers to a previous file. 
 Each integer is to be separated by a space. Close the file, then reopen the file and read the data from the file and display it.
 */
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Jonah
- */
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
@@ -33,29 +20,29 @@ public class Program2 {
         Random random = new Random();
         
         try {
-            // Create the file if it doesn't exist
+// Create the file if it doesn't exist
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
             } else {
                 System.out.println("File already exists. Appending data.");
             }
             
-            // Write or append 10 random numbers to the file
-            FileWriter writer = new FileWriter(file, true); // true = append mode
+// Write or append 10 random numbers to the file. True = append mode
+            FileWriter writer = new FileWriter(file, true);
             
             for (int i = 0; i < 10; i++) {
-                int number = random.nextInt(100); // random number 0-99
+                int number = random.nextInt(100); 
                 writer.write(number + " ");
             }
             
             writer.close();
-            System.out.println("Successfully wrote numbers to the file.");
+            System.out.println("Numbers successfully written to the file.");
             
-            // Read and display the data
+// Read and display the data to the user.
             FileReader reader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(reader);
             
-            System.out.println("Contents of the file:");
+            System.out.println("File contents:");
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
@@ -64,7 +51,7 @@ public class Program2 {
             bufferedReader.close();
             
         } catch (IOException e) {
-            System.out.println("An error occurred.");
+            System.out.println("An error has occurred.");
             e.printStackTrace();
         }
     }
